a5ab4d55b4e3c42fe9cd5dab8f55984a6edbe49f
Pole11 <riccardo.polelli02@gmail.com>
2024-02-28 14:04:40 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module it.polimi.ingsw2024polellipozziquartieritessera.ingsw2024polellipozziquartieritessera {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens it.polimi.ingsw2024polellipozziquartieritessera.ingsw2024polellipozziquartieritessera to javafx.fxml;
+    exports it.polimi.ingsw2024polellipozziquartieritessera.ingsw2024polellipozziquartieritessera;
+}
