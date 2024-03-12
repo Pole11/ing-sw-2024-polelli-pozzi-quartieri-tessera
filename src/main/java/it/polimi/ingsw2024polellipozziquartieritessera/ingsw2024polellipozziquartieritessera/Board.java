6b81d5ad6da5a94c912fee41cd7ce57d57f81882
tex <giac.tessera@gmail.com>
2024-03-12 10:42:00 +0100

    Fixed Board attributes names

--- src/main/java/it/polimi/ingsw2024polellipozziquartieritessera/ingsw2024polellipozziquartieritessera/Board.java
+++ src/main/java/it/polimi/ingsw2024polellipozziquartieritessera/ingsw2024polellipozziquartieritessera/Board.java
@@ -2,8 +2,8 @@ package it.polimi.ingsw2024polellipozziquartieritessera.ingsw2024polellipozziqua
 
 public class Board {
     private ArrayList<GoldCard> sharedGoldCards;
-    private ArrayList<ResourceCard> sharedResourceCard;
-    private ArrayList<ObjectiveCard> sharedResourceCard;
+    private ArrayList<ResourceCard> sharedResourceCards;
+    private ArrayList<ObjectiveCard> sharedObjectiveCards;
     private ArrayList<GoldCard> goldDeck;
     private ArrayList<ResourceCard> resourceDeck;
 }
