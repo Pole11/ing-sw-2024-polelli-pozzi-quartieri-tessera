e1ad10fe6303a8470140aaa846a804c5fc6593d4
FedericoQuartieri <federico.quartieri2002@gmail.com>
2024-03-19 17:08:04 +0100

    choosed GSON and implemented beginning of parser, finished cards.json, i need to rebase with master

--- src/main/java/it/polimi/ingsw2024polellipozziquartieritessera/ingsw2024polellipozziquartieritessera/Main.java
+++ src/main/java/it/polimi/ingsw2024polellipozziquartieritessera/ingsw2024polellipozziquartieritessera/Main.java
@@ -5,14 +5,18 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.BufferedReader;
 import com.google.gson.Gson;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.*;
+
+import it.polimi.ingsw2024polellipozziquartieritessera.ingsw2024polellipozziquartieritessera.enums.Resource;
+import it.polimi.ingsw2024polellipozziquartieritessera.ingsw2024polellipozziquartieritessera.model.card.*;
+
 
 public class Main {
     public static void main(String argv[]) throws IOException{
-        boolean store = existStore();
+        boolean store = existStore(); //FA persistance
         if (store){
-            //
+            // takes data from store
         } else {
             populate();
         }
@@ -47,8 +51,25 @@ public class Main {
         // path poi da mettere in config
         String jsonString = readJSON(filePath + "/src/main/java/it/polimi/ingsw2024polellipozziquartieritessera/ingsw2024polellipozziquartieritessera/cards.json");
         Gson gson = new Gson();
-        Map map = gson.fromJson(jsonString, Map.class);
-        map.entrySet().stream().forEach();
+        Map cards = gson.fromJson(jsonString, Map.class);
+        HashMap cardsMap = new HashMap<Integer, Card>();
+        for (Object key : cards.keySet()) {
+            Map card = gson.fromJson(cards.get(key).toString(), Map.class);
+            Integer id = Integer.parseInt(key.toString());
+            if (card.get("Type").equals("Objective")){
+                //
+            } else {
+                System.out.println(card.get("FrontCorner"));
+                Corner[] frontCorners = null;
+                Corner[] backCorners = null;
+                /*switch (card.get("Type")){
+                    case "Resource":
+                        cardsMap.put(id, new ResourceCard(id.intValue(), card.get("ResourceType"), card.get(("Points")), frontCorners, backCorners, centerBackResource)) );
+                }*/
+            }
+
+
+        }
 
     }
 }
