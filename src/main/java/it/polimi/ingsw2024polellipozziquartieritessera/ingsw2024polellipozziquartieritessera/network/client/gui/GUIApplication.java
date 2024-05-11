c43a075eee82381ca9822f8e1b04125cafbdeb57
Pole11 <riccardo.polelli02@gmail.com>
2024-05-12 00:22:36 +0200

    Preparation phase in GUI is ready even if is extremely ugly

--- src/main/java/it/polimi/ingsw2024polellipozziquartieritessera/ingsw2024polellipozziquartieritessera/network/client/gui/GUIApplication.java
+++ src/main/java/it/polimi/ingsw2024polellipozziquartieritessera/ingsw2024polellipozziquartieritessera/network/client/gui/GUIApplication.java
@@ -8,11 +8,13 @@ import javafx.scene.*;
 import javafx.stage.*;
 
 import java.io.*;
+import java.util.Objects;
 
 public class GUIApplication extends Application {
     private static VirtualServer server;
     private static VirtualView client;
     private static GUIController guiController;
+    private static Stage mainStage;
 
     public void setServer(VirtualServer server) {
         this.server = server;
@@ -26,14 +28,21 @@ public class GUIApplication extends Application {
         return this.guiController;
     }
 
+    public static void changeScene(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(GUIApplication.class.getResource(fxml));
+        fxmlLoader.setController(guiController);
+        mainStage.getScene().setRoot(fxmlLoader.load());
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
-        //FXMLLoader fxmlLoader = new FXMLLoader(GUIApplication.class.getResource("/it.polimi.ingsw2024polellipozziquartieritessera.ingsw2024polellipozziquartieritessera.network.client.gui/game.fxml"));
-        FXMLLoader fxmlLoader = new FXMLLoader(GUIApplication.class.getResource("/game.fxml"));
+        //FXMLLoader fxmlLoader = new FXMLLoader(GUIApplication.class.getResource("/it.polimi.ingsw2024polellipozziquartieritessera.ingsw2024polellipozziquartieritessera.network.client.gui/lobby.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(GUIApplication.class.getResource("/lobby.fxml"));
 
         fxmlLoader.setController(guiController);
+        mainStage = stage;
 
-        Scene scene = new Scene(fxmlLoader.load(), 620, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 720, 240);
         stage.setTitle("Codex Naturalis");
         stage.setScene(scene);
         stage.show();
--- src/main/java/it/polimi/ingsw2024polellipozziquartieritessera/ingsw2024polellipozziquartieritessera/network/client/gui/GUIController.java
+++ src/main/java/it/polimi/ingsw2024polellipozziquartieritessera/ingsw2024polellipozziquartieritessera/network/client/gui/GUIController.java
@@ -1,6 +1,8 @@
 package it.polimi.ingsw2024polellipozziquartieritessera.ingsw2024polellipozziquartieritessera.network.client.gui;
 
+import it.polimi.ingsw2024polellipozziquartieritessera.ingsw2024polellipozziquartieritessera.enums.Color;
 import it.polimi.ingsw2024polellipozziquartieritessera.ingsw2024polellipozziquartieritessera.enums.Command;
+import it.polimi.ingsw2024polellipozziquartieritessera.ingsw2024polellipozziquartieritessera.enums.Side;
 import it.polimi.ingsw2024polellipozziquartieritessera.ingsw2024polellipozziquartieritessera.network.client.Client;
 import it.polimi.ingsw2024polellipozziquartieritessera.ingsw2024polellipozziquartieritessera.network.client.VirtualView;
 import it.polimi.ingsw2024polellipozziquartieritessera.ingsw2024polellipozziquartieritessera.network.server.VirtualServer;
@@ -9,6 +11,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.event.*;
 
+import java.io.IOException;
 import java.rmi.RemoteException;
 
 public class GUIController {
@@ -46,6 +49,150 @@ public class GUIController {
         } catch (RemoteException e) {
             e.printStackTrace(); // TODO: remove on prod
             serverMessageLabel.setText("There was an error while adding a user");
+        }
+
+        // check if the user has logged in
+
+        // check if the game is startable
+        try {
+            GUIApplication.changeScene("/chooseStarter.fxml");
+        } catch (IOException e) {
+            // handle
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void chooseFrontStarter(ActionEvent event) {
+        chooseSideStarter(Side.FRONT);
+    }
+
+    @FXML
+    public void chooseBackStarter(ActionEvent event) {
+        chooseSideStarter(Side.BACK);
+    }
+
+    private void chooseSideStarter(Side side) {
+        String message = Command.CHOOSESTARTER + " " + side;
+        try {
+            Client.manageInput(this.server, message.split(" "), this.client);
+        } catch (RemoteException e) {
+            e.printStackTrace(); // TODO: remove on prod
+            serverMessageLabel.setText("There was an error while choosing the starter side");
+        }
+
+        try {
+            GUIApplication.changeScene("/chooseColor.fxml");
+        } catch (IOException e) {
+            // handle
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void chooseBlueColor(ActionEvent event) {
+        chooseColor(Color.BLUE);
+    }
+
+    @FXML
+    private void chooseGreenColor(ActionEvent event) {
+        chooseColor(Color.GREEN);
+    }
+
+    @FXML
+    private void chooseYellowColor(ActionEvent event) {
+        chooseColor(Color.YELLOW);
+    }
+
+    @FXML
+    private void chooseRedColor(ActionEvent event) {
+        chooseColor(Color.RED);
+    }
+
+    private void chooseColor(Color color) {
+        String message = Command.CHOOSECOLOR + " " + color;
+        try {
+            Client.manageInput(this.server, message.split(" "), this.client);
+        } catch (RemoteException e) {
+            e.printStackTrace(); // TODO: remove on prod
+            serverMessageLabel.setText("There was an error while adding a user");
+        }
+    }
+
+    @FXML
+    private void goToChooseObjective(ActionEvent event) {
+        try {
+            GUIApplication.changeScene("/chooseObjective.fxml");
+        } catch (IOException e) {
+            // handle
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void goToChooseColor(ActionEvent event) {
+        try {
+            GUIApplication.changeScene("/chooseColor.fxml");
+        } catch (IOException e) {
+            // handle
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void goToChooseStarter(ActionEvent event) {
+        try {
+            GUIApplication.changeScene("/chooseStarter.fxml");
+        } catch (IOException e) {
+            // handle
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void goToLobby(ActionEvent event) {
+        try {
+            GUIApplication.changeScene("/lobby.fxml");
+        } catch (IOException e) {
+            // handle
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void goToGame(ActionEvent event) {
+        try {
+            GUIApplication.changeScene("/game.fxml");
+        } catch (IOException e) {
+            // handle
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void chooseFirstObjective(ActionEvent event) {
+        chooseObjective(0);
+    }
+
+    @FXML
+    public void chooseSecondObjective(ActionEvent event) {
+        chooseObjective(1);
+    }
+
+    public void chooseObjective(int index) {
+        String message = Command.CHOOSEOBJECTIVE + " " + index;
+        try {
+            Client.manageInput(this.server, message.split(" "), this.client);
+        } catch (RemoteException e) {
+            e.printStackTrace(); // TODO: remove on prod
+            serverMessageLabel.setText("There was an error while adding a user");
+        }
+
+        try {
+            GUIApplication.changeScene("/game.fxml");
+        } catch (IOException e) {
+            // handle
+            e.printStackTrace();
         }
     }
 
